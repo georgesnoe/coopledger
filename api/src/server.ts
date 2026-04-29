e55b25af8bece522c80ab3470e42f3fbeeb55e63
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-04-29 14:57:10 +0000

    config: setup socket.io

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,15 +1,21 @@
 import { createServer } from "node:http";
 import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { Server } from "socket.io";
 import { auth } from "@/lib/auth";
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
+});
+
+io.on("connection", (_) => {
+  console.log("A user connected");
 });
 
 server.listen(process.env.PORT || 3000, () => {
