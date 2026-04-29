10e06a60562b32f64db5356a2c52243cabd02ae6
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-04-29 14:42:10 +0000

    chore: rename main.ts -> server.ts

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,8 +1,12 @@
 import { createServer } from "node:http";
+import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { auth } from "@/lib/auth";
 
 const app = express();
 const server = createServer(app);
+
+app.all("/api/auth/{*any}", toNodeHandler(auth));
 
 app.get("/", (_, res) => {
   res.send("Hello World!");
