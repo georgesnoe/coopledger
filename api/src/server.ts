8295d1b6bf506601dd84269d0e67a1395b3b6049
Kossi Georges-Noé AHOMBO <georges.ahombo@gmail.com>
2026-04-29 22:53:46 +0000

    config: setup ioredis

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,12 +1,16 @@
 import { createServer } from "node:http";
 import { toNodeHandler } from "better-auth/node";
 import express from "express";
+import { Redis } from "ioredis";
 import { Server } from "socket.io";
 import { auth } from "@/lib/auth";
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const redis = new Redis(process.env.REDIS_URL as string, {
+  maxRetriesPerRequest: null,
+});
 
 app.all("/api/auth/{*any}", toNodeHandler(auth));
 
